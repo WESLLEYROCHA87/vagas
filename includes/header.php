157d0ca37974e6e0ad128b8c22e3b3004b99bd07
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-06-08 10:12:12 -0300

    Estrutura pronta.

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Vagas</title>
+</head>
+
+<body class="bg-dark text-light">
+
+    <div class="container-fluid">
+        <div class="mt-3">
+            <div class="row">
+                <div class=" col">
+                    <img src="img/LogoCoziindCorBranco.png" class="img-fluid float-xl-left mx-auto d-block" style="width: 120px;">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
